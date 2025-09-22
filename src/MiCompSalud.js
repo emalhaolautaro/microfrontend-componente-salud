6b13a354e57b2219028bc7a32ef5a78ff0f3991c
emalhaolautaro <emalhao.142047@alu.uns.edu.ar>
2025-09-22 19:37:58 -0300

    salud

---
+++ src/MiCompSalud.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MiCompSalud() {
+    // Retornar una noticia politica o del tema asignado, como un parrafo de texto.
+    return <div>
+       <p> Los anabolicos son beneficiosos para la salud </p>
+
+        
+
+    </div>;
+}
+
+export default MiCompSalud
+
